aaf6ec914964d71d4f0e495fc289a71bf96a536a
Kokou Jacques AGBLA <agblajack@gmail.com>
2024-03-04 07:48:51 +0000

    Modification du validator de update

--- src/Core/Application/Ize/TypeChambres/UpdateTypeChambreRequestValidator.cs
+++ src/Core/Application/Ize/TypeChambres/UpdateTypeChambreRequestValidator.cs
@@ -5,17 +5,6 @@ public class UpdateTypeChambreRequestValidator : CustomValidator<UpdateTypeChamb
 {
     public UpdateTypeChambreRequestValidator(IReadRepository<TypeChambre> typeChambreRepo, IStringLocalizer<UpdateTypeChambreRequestValidator> T)
     {
-            RuleFor(tc => tc.Code)
-                .NotEmpty()
-                .MaximumLength(12)
-                .MustAsync(async (code, ct) => await typeChambreRepo.FirstOrDefaultAsync(new TypeChambreByCodeSpec(code), ct) is null)
-                    .WithMessage((_, code) => T["Le type chambre avec le code {0} existe déjà", code]);
-
-            RuleFor(tc => tc.Libelle)
-                .NotEmpty()
-                .MaximumLength(100)
-                .MustAsync(async (libelle, ct) => await typeChambreRepo.FirstOrDefaultAsync(new TypeChambreByLibelleSpec(libelle), ct) is null)
-                    .WithMessage((_, libelle) => T["Le type chambre {0} existe déjà", libelle]);
 
     }
 }
