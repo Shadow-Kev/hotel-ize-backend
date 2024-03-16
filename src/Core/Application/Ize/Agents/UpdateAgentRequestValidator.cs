e724987641de00e6fecce82e8109d319d949bcce
Kokou Jacques AGBLA <agblajack@gmail.com>
2024-03-16 12:59:51 +0000

    update agent fix bug

--- src/Core/Application/Ize/Agents/UpdateAgentRequestValidator.cs
+++ src/Core/Application/Ize/Agents/UpdateAgentRequestValidator.cs
@@ -1,4 +1,10 @@
-﻿namespace FSH.WebApi.Application.Ize.Agents;
-public class UpdateAgentRequestValidator
+﻿using FSH.WebApi.Domain.Ize;
+
+namespace FSH.WebApi.Application.Ize.Agents;
+public class UpdateAgentRequestValidator : CustomValidator<UpdateAgentRequest>
 {
+    public UpdateAgentRequestValidator(IReadRepository<Agent> agentRepo, IStringLocalizer<UpdateAgentRequestValidator> T)
+    {
+
+    }
 }
