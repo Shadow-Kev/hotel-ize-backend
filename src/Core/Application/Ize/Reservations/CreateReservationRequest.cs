74c4d0b68ff1354faf74762f71c5fb7cc8ddb03e
AGBLA kokou Jacques <agblajack@gmail.com>
2024-06-01 11:32:42 +0000

    fix create reservation

--- src/Core/Application/Ize/Reservations/CreateReservationRequest.cs
+++ src/Core/Application/Ize/Reservations/CreateReservationRequest.cs
@@ -29,7 +29,7 @@ public class CreateReservationRequestHandler : IRequestHandler<CreateReservation
     {
 
         var reservation = new Reservation(request.Nom, request.Prenom, request.ChambreId, request.TypeReservationId, request.StatutReservation,  request.DateArrive);
-        if(request.StatutReservation == Statut.RESERVE && request.ChambreId != null)
+        if(request.StatutReservation == Statut.RESERVE && request.ChambreId != null && request.DateArrive == DateTime.Now)
         {
             await _mediator.Send(new UpdateChambreStatutRequest
             {
