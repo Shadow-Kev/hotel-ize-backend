bdda59d3953159a4eb451b483caa40dbc31a8136
Kevin-Joel Agbeto <agbetokevin@gmail.com>
2024-07-07 09:45:11 +0000

    New facture done !

--- src/Infrastructure/Common/Extensions/QuestPdfExtensions.cs
+++ src/Infrastructure/Common/Extensions/QuestPdfExtensions.cs
@@ -17,5 +17,18 @@ public static class QuestPdfExtensions
     public static void LabelCell(this IContainer container, string text) => container.Cell(true).Text(text).Medium();
 
     // allows you to inject any type of content, e.g. image
-    public static IContainer ValueCell(this IContainer container) => container.Cell(false);
+    public static void ValueCell(this IContainer container, string text) => container.Cell(false).Text(text);
+
+    // for simplicity, you can also use extension method described in the "Extending DSL" section
+    public static IContainer Block(IContainer container)
+    {
+        return container
+            .Border(1)
+            .Background(Colors.Grey.Lighten3)
+            .ShowOnce()
+            .MinWidth(50)
+            .MinHeight(50)
+            .AlignCenter()
+            .AlignMiddle();
+    }
 }
--- src/Infrastructure/Common/Services/PdfServices.cs
+++ src/Infrastructure/Common/Services/PdfServices.cs
@@ -1,6 +1,7 @@
 ﻿using FSH.WebApi.Application.Common.Interfaces;
 using FSH.WebApi.Application.Ize.Clients;
 using FSH.WebApi.Application.Ize.Ventes;
+using FSH.WebApi.Infrastructure.Common.Extensions;
 using MediatR;
 using Microsoft.Extensions.Configuration;
 using QuestPDF.Fluent;
@@ -91,7 +92,7 @@ public class PdfServices : IPdfService
             grid.Item(6).AlignCenter().Column(c =>
             {
                 c.Item().Text("HOTEL IZE").FontSize(20);
-                c.Item().Text($"Tél: 00 228 90 04 97 / 90 04 97 /90 04 97").FontSize(12);
+                c.Item().Text($"Tél: 00 228 90 43 98 98/ 96 04 98 98").FontSize(12);
                 c.Item().Text($"260 BP: 1097 Kpalimé - TOGO").FontSize(12);
             });
 
@@ -129,15 +130,46 @@ public class PdfServices : IPdfService
 
             column.Item().Element(con => ComposeTableClient(con, model));
 
-            var totalPrice = CalculerPrixTotalClient(model);
-            column.Item().AlignRight().Text($"Total: {totalPrice}FCFA").FontSize(14);
+            column.Item().Row(row =>
+            {
+
+            });
+
+            column.Item().Row(row =>
+            {
+                row.RelativeItem().AlignLeft().Text("Signature du caissier").Bold();
+                row.RelativeItem().AlignRight().Text("Signature du client").Bold();
+            });
+            //var totalPrice = CalculerPrixTotalClient(model);
+            //column.Item().AlignRight().Text($"Total: {totalPrice}FCFA").FontSize(14);
         });
     }
 
     public decimal CalculerPrixTotalClient(ClientDetailsDto client)
     {
         decimal total = 0;
-        total = client.Chambre.Prix;
+        total = client.Chambre.Prix * CalculerNombreDeNuites(client);
+
+        if (client.Ventes is not null)
+        {
+            foreach (var vente in client.Ventes)
+            {
+                if (vente.VenteProduits is not null)
+                {
+                    foreach (var venteProduit in vente.VenteProduits)
+                    {
+                        total += venteProduit.Quantite * venteProduit.Prix;
+                    }
+                }
+            }
+        }
+
+        return Math.Round(total);
+    }
+
+    public decimal CalculerPrixTotalBarClient(ClientDetailsDto client)
+    {
+        decimal total = 0;
 
         if (client.Ventes is not null)
         {
@@ -227,123 +259,50 @@ public class PdfServices : IPdfService
         {
             table.ColumnsDefinition(columns =>
             {
-                columns.ConstantColumn(50);
-                columns.RelativeColumn(3);
+                columns.ConstantColumn(100);
                 columns.RelativeColumn();
-                columns.RelativeColumn();
+                columns.ConstantColumn(100);
                 columns.RelativeColumn();
             });
 
-            table.Header(header =>
-            {
-                header.Cell().Element(CellStyle).Text("#");
-                header.Cell().Element(CellStyle).Text("Désignation");
-                header.Cell().Element(CellStyle).AlignRight().Text("Prix Unitaire");
-                header.Cell().Element(CellStyle).AlignRight().Text("Quantité");
-                header.Cell().Element(CellStyle).AlignRight().Text("Prix Total");
-
-                static IContainer CellStyle(IContainer container)
-                {
-                    return container.DefaultTextStyle(x => x.SemiBold()).PaddingVertical(5).BorderBottom(1).BorderColor(Colors.Black);
-                }
-            });
-
-            int index = 1;
             int nbreNuite = CalculerNombreDeNuites(model);
-            int tsp = 1000 * nbreNuite;
+            int tspt = 1000 * nbreNuite;
+            decimal totalBar = CalculerPrixTotalBarClient(model);
+            decimal totalAPayer = CalculerPrixTotalClient(model);
 
-            // Ajoutez ici la ligne pour la TSPT
-            table.Cell().Element(CellStyle).Text("TSPT");
-            table.Cell().Element(CellStyle).Text("");
-            table.Cell().Element(CellStyle).AlignRight().Text("1000FCFA");
-            table.Cell().Element(CellStyle).AlignRight().Text("");
-            table.Cell().Element(CellStyle).AlignRight().Text($"{tsp}FCFA");
+            table.ExtendLastCellsToTableBottom();
 
-            // Ajoutez ici la ligne pour la chambre
-            table.Cell().Element(CellStyle).Text("Chambre");
-            table.Cell().Element(CellStyle).Text(model.Chambre?.Nom ?? "");
-            table.Cell().Element(CellStyle).AlignRight().Text($"{Math.Round((decimal)model.Chambre?.Prix!)}FCFA");
-            table.Cell().Element(CellStyle).AlignRight().Text("");
-            table.Cell().Element(CellStyle).AlignRight().Text($"{CalculerPrixHT((decimal)model.Chambre?.Prix!, nbreNuite)}FCFA");
-            static IContainer CellStyle(IContainer container)
-            {
-                return container.BorderBottom(1).BorderColor(Colors.Grey.Lighten2).PaddingVertical(5);
-            }
+            table.Cell().RowSpan(3).LabelCell("Payé par");
+            table.Cell().RowSpan(3).ValueCell($"{model.Nom} {model.Prenom}");
 
-            foreach (var vente in model.Ventes)
-            {
-                if (vente.VenteProduits is not null)
-                {
-                    foreach (var item in vente.VenteProduits)
-                    {
-                        table.Cell().Element(CellStyle).Text(index++.ToString());
-                        table.Cell().Element(CellStyle).Text(item.Product?.Name ?? "");
-                        table.Cell().Element(CellStyle).AlignRight().Text($"{Math.Round(item.Prix)}FCFA");
-                        table.Cell().Element(CellStyle).AlignRight().Text(item.Quantite.ToString());
-                        table.Cell().Element(CellStyle).AlignRight().Text($"{Math.Round(item.Prix * item.Quantite)}FCFA");
+            table.Cell().LabelCell("Numéro de chambre");
+            table.Cell().ValueCell($"{model.Chambre.Nom}");
 
-                        //static IContainer CellStyle(IContainer container)
-                        //{
-                        //    return container.BorderBottom(1).BorderColor(Colors.Grey.Lighten2).PaddingVertical(5);
-                        //}
-                    }
-                }
-            }
+            table.Cell().LabelCell("Nombre de nuité");
+            table.Cell().ValueCell($"{nbreNuite}");
+
+            table.Cell().LabelCell("Téléphone");
+            table.Cell().ValueCell($"{model.Contact}");
+
+            table.Cell().ColumnSpan(2).LabelCell("Date");
+            table.Cell().ColumnSpan(1).ValueCell($"Du {model.DateArrive.Value.ToString("d")}");
+            table.Cell().ColumnSpan(1).ValueCell($"Au {model.DateDepart.Value.ToString("d")}");
+
+            table.Cell().ColumnSpan(3).LabelCell("TSPT");
+            table.Cell().ColumnSpan(1).ValueCell($"{tspt} FCFA");
+
+            table.Cell().ColumnSpan(2).LabelCell("Chambre");
+            table.Cell().ColumnSpan(1).ValueCell($"{model.Chambre.Prix.ToString()} FCFA");
+            table.Cell().ColumnSpan(1).ValueCell($"{model.Chambre.Prix * nbreNuite} FCFA");
+
+            table.Cell().ColumnSpan(3).LabelCell("BAR");
+            table.Cell().ColumnSpan(1).ValueCell($"{totalBar} FCFA");
+
+            table.Cell().ColumnSpan(3).LabelCell("TOTAL A PAYER");
+            table.Cell().ColumnSpan(1).ValueCell($"{totalAPayer} FCFA");
+
         });
     }
-
-    //public void ComposeTableClient(IContainer container, ClientDetailsDto model)
-    //{
-    //    container.Table(table =>
-    //    {
-    //        table.ColumnsDefinition(columns =>
-    //        {
-    //            columns.ConstantColumn(25);
-    //            columns.RelativeColumn(3);
-    //            columns.RelativeColumn();
-    //            columns.RelativeColumn();
-    //            columns.RelativeColumn();
-    //        });
-
-    //        table.Header(header =>
-    //        {
-    //            header.Cell().Element(CellStyle).Text("#");
-    //            header.Cell().Element(CellStyle).Text("Désignation");
-    //            header.Cell().Element(CellStyle).AlignRight().Text("Prix Unitaire");
-    //            header.Cell().Element(CellStyle).AlignRight().Text("Quantité");
-    //            header.Cell().Element(CellStyle).AlignRight().Text("Prix Total");
-
-    //            static IContainer CellStyle(IContainer container)
-    //            {
-    //                return container.DefaultTextStyle(x => x.SemiBold()).PaddingVertical(5).BorderBottom(1).BorderColor(Colors.Black);
-    //            }
-    //        });
-
-    //        if (model.Ventes is not null)
-    //        {
-    //            int index = 1;
-    //            foreach (var vente in model.Ventes)
-    //            {
-    //                if (vente.VenteProduits is not null)
-    //                {
-    //                    foreach (var item in vente.VenteProduits)
-    //                    {
-    //                        table.Cell().Element(CellStyle).Text(index++.ToString());
-    //                        table.Cell().Element(CellStyle).Text(item.Product?.Name ?? "");
-    //                        table.Cell().Element(CellStyle).AlignRight().Text($"{item.Prix}FCFA");
-    //                        table.Cell().Element(CellStyle).AlignRight().Text(item.Quantite.ToString());
-    //                        table.Cell().Element(CellStyle).AlignRight().Text($"{item.Prix * item.Quantite}FCFA");
-
-    //                        static IContainer CellStyle(IContainer container)
-    //                        {
-    //                            return container.BorderBottom(1).BorderColor(Colors.Grey.Lighten2).PaddingVertical(5);
-    //                        }
-    //                    }
-    //                }
-    //            }
-    //        }
-    //    });
-    //}
 
     public async Task<string> GenerateClientInvoice(Guid id)
     {
@@ -359,11 +318,14 @@ public class PdfServices : IPdfService
         {
             doc.Page(page =>
             {
+                page.DefaultTextStyle(ts => ts.FontFamily("Times New Roman"));
                 page.Margin(50);
                 page.Header().Element(con => ComposeHeaderClient(con, client));
                 page.Content().Element(con => ComposeContentClient(con, client));
                 page.Footer().AlignCenter().Text(x =>
                 {
+                    x.Span("Merci pour votre visite et à la prochaine !");
+                    x.EmptyLine();
                     x.CurrentPageNumber();
                     x.Span(" / ");
                     x.TotalPages();
